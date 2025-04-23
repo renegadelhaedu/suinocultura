1f883a20480ad638e680ce806ea54aa66d26caf8
renegadelhaedu <renegadelha.edu@gmail.com>
2025-04-23 16:19:27 -0300

    versao1.2

--- RC522v2.ino
+++ RC522v2.ino
@@ -1,5 +1,21 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
  
 #define SS_PIN 10
 #define RST_PIN 9
@@ -16,6 +32,16 @@ void setup()
   SPI.begin();		// Inicia  SPI bus
   mfrc522.PCD_Init();	// Inicia MFRC522
   mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); 
+  }
+  display.clearDisplay();
+  display.setTextSize(2); 
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.display();
   Serial.println("Aproxime o seu cartao do leitor...");
   Serial.println();
 }
@@ -57,6 +83,14 @@ void loop()
   conteudo.toUpperCase();
   Serial.println (conteudo.substring(1));
   beep ();
+  display.clearDisplay();
+  display.setTextSize(2); 
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print("ID: \n");
+  conteudo.replace(" ", "");
+  display.print(conteudo);
+  display.display();
   delay (1000);
 } 
  
